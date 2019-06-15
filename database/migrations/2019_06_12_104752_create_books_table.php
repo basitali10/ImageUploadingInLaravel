eaf049f633561db241914be4bc8f9ed1c2eeeaa7
Basit Ali <basitalikhatri98@gmail.com>
2019-06-15 13:38:35 +0500

    Migration of Books Modified.

--- database/migrations/2019_06_12_104752_create_books_table.php
+++ database/migrations/2019_06_12_104752_create_books_table.php
@@ -17,7 +17,6 @@ class CreateBooksTable extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->string('filename')->nullable();
             $table->string('mime')->nullable();
-            $table->string('original_filename')->nullable();
         });
     }
 
